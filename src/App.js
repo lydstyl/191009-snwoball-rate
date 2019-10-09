1810bb36e9cdd223b3ac12aa67cc7a2a8fe17904
Gabriel Brun <lydstyl@gmail.com>
2019-10-09 22:49:00 +0200

    form working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  constructor() {
+    super();
 
-export default App;
+    this.state = {
+      originValue: 100,
+      rate: 0.05,
+      occurrenceNumber: 10
+    };
+  }
+
+  inputChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Snowball Rate</h1>
+
+        <form className='preferences'>
+          <div className='preference'>
+            <label>rate?</label>
+            <input
+              type='number'
+              name='rate'
+              value={this.state.rate}
+              onChange={e => this.inputChange(e)}
+            />
+          </div>
+          <div className='preference'>
+            <label>occurrenceNumber?</label>
+            <input
+              type='number'
+              name='occurrenceNumber'
+              value={this.state.occurrenceNumber}
+              onChange={e => this.inputChange(e)}
+            />
+          </div>
+        </form>
+        <div className='chartBox'>chartBox</div>
+      </div>
+    );
+  }
+}
